a544a1793bc68042c1c9b5901d4eff6f063fb170
Hampus Nilsson <hampus.beus@gmail.com>
2020-11-16 22:10:03 +0100

    add middleware handler

--- Server.js
+++ Server.js
@@ -7,7 +7,9 @@ dotenv.config();
 const app = express();
 app.use(helmet());
 app.use(morgan('common'));
+
 // app.use(isAuthenticated);
+
 const port = process.env.PORT;
 
 app.get('/test', (request, response) => {
@@ -16,11 +18,19 @@ app.get('/test', (request, response) => {
 
 app.get('/user', isAuthenticated, (request, response) => {});
 
+app.use(notFound);
+
 function isAuthenticated(request, response, next) {
   request.query.admin === 'true'
     ? response.send('You are an admin')
     : response.send('You cannot make call to this API URL');
   next();
+}
+
+function notFound(request, response, next) {
+  const error = new Error(`Not found: ${request.originalUrl}`);
+  response.status(404);
+  next(error);
 }
 
 app.listen(port, () => {
