83ece52f86910b8d1a2eff6eafa4ae73cdce39e2
Hampus Nilsson <hampus.beus@gmail.com>
2020-11-30 21:14:06 +0100

    add changes

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,4 +1,5 @@
 import userModel from '../models/User.model.js';
+import UserRoutes from '../routes/User.routes.js';
 
 const createUser = async (req, res) => {
   const user = new userModel({
@@ -13,6 +14,43 @@ const createUser = async (req, res) => {
   }
 };
 
+const getAllUser = async (req, res) => {
+  try {
+    const response = await userModel.find();
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+const getUserWithId = async (req, res) => {
+  try {
+    const response = await userModel.findById(req.params.userId);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(500).send({
+      message: 'Error trying to retrieve use with ID ' + req.params.userId,
+      error: error.message,
+    });
+  }
+};
+
+const getUserWithUsername = async (req, res) => {
+  try {
+    const response = await userModel.find({ username: req.query.username });
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(500).send({
+      message:
+        'Error occured while trying to retrieve user' + req.query.username,
+      error: error.message,
+    });
+  }
+};
+
 export default {
   createUser,
+  getAllUser,
+  getUserWithId,
+  getUserWithUsername,
 };
--- src/routes/User.routes.js
+++ src/routes/User.routes.js
@@ -1,7 +1,11 @@
+import UserController from '../controllers/User.controller.js';
 import userController from '../controllers/User.controller.js';
 
 const routes = (app) => {
   app.post('/user', userController.createUser);
+  app.get('/user', userController.getAllUser);
+  app.get('/user/:userId', UserController.getUserWithId);
+  app.get('/searchuser', UserController.getUserWithUsername);
 };
 
 export default {
